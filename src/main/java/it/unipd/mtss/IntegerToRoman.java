1d85121ac6bb671fd3f5c609064992c5648f4560
Archimedes-BiH <bilalsabic14@gmail.com>
2025-05-04 19:34:52 +0200

    IntegerToRoman, close #2

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -3,3 +3,27 @@
 // Bilal Sabic 2111022
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number) {
+        if (number < 1 || number > 1000) {
+            throw new IllegalArgumentException("Il numero deve essere compreso tra 1 e 1000");
+        }
+
+        // Definizione delle lettere romane e dei loro valori corrispondenti
+        String[] romanLetters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        StringBuilder romanNumber = new StringBuilder();
+
+        // Itera attraverso i valori romani e aggiungi le lettere corrispondenti
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                number -= values[i];
+                romanNumber.append(romanLetters[i]);
+            }
+        }
+
+        return romanNumber.toString();
+    }
+}
